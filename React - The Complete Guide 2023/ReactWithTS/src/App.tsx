4fa4e89b1a484c74361a798a764828e355977f22
Rowan Gauld <rowangauld1@gmail.com>
2023-07-28 01:24:16 +0200

    TS -react

---
+++ React - The Complete Guide 2023/ReactWithTS/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ToDos from './components/ToDos';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDos/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React - The Complete Guide 2023/ReactWithTS/src/components/ToDos.tsx
@@ -0,0 +1,7 @@
+function ToDos(){
+        return (<ul>
+<li>#1</li>
+    </ul>);
+}
+
+export default ToDos;
